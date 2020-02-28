3ed36c6c2f3811590d75d846b19f28997e3fbd6f
sylvie54 <sylvie.tcht@gmail.com>
2020-02-28 20:11:59 +0100

    first commit

---
+++ src/main/java/AFPA/DEMO03/demo/essaiLogin/EssaiLoginApplication.java
@@ -0,0 +1,13 @@
+package AFPA.DEMO03.demo.essaiLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssaiLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssaiLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AFPA/DEMO03/demo/essaiLogin/EssaiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package AFPA.DEMO03.demo.essaiLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssaiLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
